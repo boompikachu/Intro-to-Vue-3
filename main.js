3128bd533e97582b3a1745b709b931c72b668a33
Patthanat Thanintantrakun <boomeevee@icloud.com>
2020-12-11 15:07:19 +0700

    done

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
-        }
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 1000, color: "green" },
+        { id: 1001, color: "blue" },
+      ],
+      sizes: [
+        { id: 1, letter: "S" },
+        { id: 2, letter: "M" },
+        { id: 3, letter: "L" },
+        { id: 4, letter: "XL" },
+      ],
     }
+  },
 })
